6712e479dc0c39ce56b424a5c64e671a3f6dceb3
superTO <to840116@gmail.com>
2021-03-17 23:35:15 +0800

    style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) }, { path: 'home', loadChildren: () => import('./markdown/markdown.module').then(m => m.MarkdownModule) }];
+const routes: Routes = [
+  { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  { path: 'home', loadChildren: () => import('./markdown/markdown.module').then(m => m.MarkdownModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
